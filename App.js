74d0f35289eafa73cfa357488784fed6891fd238
Dmitriy7471 <dabocharov1974@gmail.com>
2025-06-01 14:08:32 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import Header from './component/header/header';
+import BottomHeader from './component/header/bottom-header';
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <BottomHeader/>
+      
+    </>
+  );
+}
+
+export default App;
